bb2a3257617355475418c974c978b2467d335da6
1021255794@qq.com <1021255794@qq.com>
2019-05-11 14:44:45 +0800

    be -2 基本功能完成

--- app.js
+++ app.js
@@ -10,20 +10,29 @@ const server = app.listen(port, () => {
   console.log('server listening in port', port)
 })
 
-const io = SocketIO
+//拿到io
+const io = SocketIO(server)
 
 // app.use(express.static(path.join(__dirname,'./dist')))
 const pixelData = [
   //每个颜色先保存成二位数组
-  ['red', 'red', 'blue', 'black']
-  ['red', 'red', 'blue', 'black']
-  ['red', 'red', 'blue', 'black']
-  ['red', 'red', 'blue', 'black']
+  ['red', 'red', 'blue', 'black'],
+  ['red', 'red', 'blue', 'black'],
+  ['red', 'red', 'blue', 'black'],
+  ['red', 'red', 'blue', 'black'],
 ]
-io.on('connection',(ws) => {
+io.on('connection',(socket) => {
   //前端io通过这句收到事件
-  ws.emit('pixel-data', pixelData)
-  ws.on('disconnect', () => {
+  socket.emit('pixel-data', pixelData)
+
+  socket.on('draw-dot', ({row,col,color}) => {
+    console.log('14211')
+    pixelData[row][col] = color
+    // socket.broadcast.emit('update-dot', { row, col, color })//把这个点发给所有的连接上来的客户端
+    console.log('0200')
+    io.emit('update-dot', { row, col, color })  //给别人发一份也给自己发一份
+  })
+  socket.on('disconnect', () => {
     console.log('someone leave')
   })
 })
